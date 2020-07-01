9f4d6abb46f53b92978a9751a79a987c1e4493da
pp641 <43077034+pp641@users.noreply.github.com>
2020-07-01 12:19:08 +0530

    Create gfg-6 first_n_fibbonacci.cpp

---
+++ my_gfg_solutions/gfg-6 first_n_fibbonacci.cpp
@@ -0,0 +1,23 @@
+int main()
+{
+    long long int n,t,j,i;
+    cin>>t;
+    for(j=0;j<t;j++)
+{
+    cin>>n;
+    long long int fib[n];
+    fib[0]=1;
+    fib[1]=1;
+    for(i=2;i<n;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+
+     for(i=0;i<n;i++)
+    {
+        cout<<fib[i]<<" ";
+    }
+    cout<<endl;
+}
+    return 0;
+}
