822d6e23f9a110467b395faf542da9ad5cd85f42
pp641 <43077034+pp641@users.noreply.github.com>
2020-07-01 12:18:32 +0530

    Create gfg-7 insertion_in_linked_list.cpp

---
+++ my_gfg_solutions/gfg-7 insertion_in_linked_list.cpp
@@ -0,0 +1,78 @@
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) { 
+    data = x;
+    next = NULL;
+  }
+};
+
+void printList(Node *head) { 
+    Node *node = head;
+    while (node != NULL) {
+      cout << node -> data << ' ';
+      node = node -> next;
+    }
+    cout << endl;
+}
+
+Node *insertAtBegining(Node *head, int newData);
+Node *insertAtEnd(Node *head, int newData);
+
+
+int main() {
+  int test;
+  cin >> test; 
+  while (test--) { 
+       int n, ch, tmp;
+    Node *head = NULL; 
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      cin >> tmp >> ch; 
+      if (ch) head = insertAtEnd(head, tmp); 
+      else head = insertAtBegining(head, tmp);
+    }
+    printList(head);
+  }
+  return 0;
+}
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+Node *insertAtBegining(Node *head, int newData)
+{ 
+            
+    
+            Node *temp = new Node(newData);
+            temp->next = head;
+            head =temp;
+            return head;
+}
+
+
+Node *insertAtEnd(Node *head, int newData) 
+{
+    if(head == NULL)
+            return new Node(newData);
+    else
+        {
+            Node *temp = new Node(newData);
+            Node *x = head;
+        while(x->next!= NULL)
+            {
+                 x = x->next;
+            }
+            x->next=temp;
+            return head;
+        }
+    
+}
